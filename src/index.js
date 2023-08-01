d81107c880baf781fb34d8331a814dd8da926e7c
Nikita0708 <91466168+Nikita0708@users.noreply.github.com>
2023-08-01 23:41:05 +0200

    ok

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import Contacts from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Contacts />
   </React.StrictMode>
 );
